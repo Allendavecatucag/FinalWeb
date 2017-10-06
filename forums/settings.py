28df123d0d52b8d02308c883c9709c1efe878818
allendavecatucag <catucagallenit1b@gmail.com>
2017-10-06 17:05:07 +0800

    hey commit

--- forums/settings.py
+++ forums/settings.py
@@ -150,15 +150,15 @@ if os.getcwd() == '/app':
     os.path.join(BASE_DIR, 'static'),
     )
 
-    # # Update database configuration with $DATABASE_URL.
-    # import dj_database_url
-    # db_from_env = dj_database_url.config()
-    # DATABASES['default'].update(db_from_env)
+    # Update database configuration with $DATABASE_URL.
+    import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
     
-    # # Update database configuration with $DATABASE_URL.
-    # import dj_database_url
-    # db_from_env = dj_database_url.config(conn_max_age=500)
-    # DATABASES['default'].update(db_from_env)
+    # Update database configuration with $DATABASE_URL.
+    import dj_database_url
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 
     # Static files (CSS, JavaScript, Images)
