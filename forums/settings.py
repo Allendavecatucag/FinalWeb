849f7aa34bdfedd412d12b79b6face5b6115d68f
allendavecatucag <catucagallenit1b@gmail.com>
2017-10-06 16:12:44 +0800

    hey commit

--- forums/settings.py
+++ forums/settings.py
@@ -166,13 +166,13 @@ if os.getcwd() == '/app':
 
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-    # STATIC_URL = '/static/'
+    STATIC_URL = '/static/'
 
-    # # Extra places for collectstatic to find static files.
-    # STATICFILES_DIRS = (
-    #     os.path.join(PROJECT_ROOT, 'static'),
-    # )
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
 
-    # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
