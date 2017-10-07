e7aa0a240d9ce9a0d1f31249eb5b5a480188cc32
allendavecatucag <catucagallenit1b@gmail.com>
2017-10-08 01:29:21 +0800

    heroku deploy nabi!

--- forum/admin.py
+++ forum/admin.py
@@ -6,20 +6,18 @@ from .models import User
 # Register your models here.
 admin.site.register(Post)
 
-class MyUserCreationForm(UserCreationForm):
-	def clean_username(self):
-		username = self.cleaned_data["username"]
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
         try:
-        	User._default_manager.get(username=username)
-        except User.DoesNotExist:
+            CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
             return username
         raise forms.ValidationError(self.error_messages['duplicate_username'])
- 
-class Meta(UserCreationForm.Meta):
-	model = User
- 
- 
-class UserAdmin(AuthUserAdmin):
-	create_form_class = UserCreationForm
-    add_form = MyUserCreationForm
-    update_form_class = UserChangeForm
