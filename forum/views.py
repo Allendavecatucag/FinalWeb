bdbeb584d85e07fdc236527eadbd82518c2e5cf5
allendavecatucag <catucagallenit1b@gmail.com>
2017-10-08 20:43:36 +0800

    deploy na uy

--- forum/views.py
+++ forum/views.py
@@ -14,7 +14,8 @@ from django.contrib.auth.models import User
 def index(request):
     try:
         admin = User.objects.get(id=2)
-    
+    except World.DoesNotExist:
+    pass
     # admin = User.objects.get(id=2)
     tops = Post.objects.filter(author=admin)
     post_list = Post.objects.exclude(author=admin).order_by('-date_added')
